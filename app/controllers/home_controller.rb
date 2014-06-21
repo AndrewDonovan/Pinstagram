4833da777483a05a21de51c09fdbfef783af5d49
Andrew Donovan <andrewdonovan@Andrews-MacBook-Air.local>
2014-06-20 17:56:51 -0700

    cleaned code and removed everything not needed and started commeting everything

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
-
   before_action :get_user_id
-
   def index
   		if params[:query] 
         user = Instagram.user_search(params[:query])[0].id
@@ -17,48 +15,36 @@ class HomeController < ApplicationController
   			@user_name_Name2 = Home.nameFind(params[:keyword])
   		end
   	    @instagram = Instagram.user_recent_media("12278927", {:count => 15})
-
   	  if params[:hashword]
 		    @tags = Instagram.tag_recent_media((params[:hashword]), {:count => 15, :client_id => ENV['INSTAAPI_CLIENT_ID']})
    	  end
-
   	  if params[:hashword2]
 		    @tags2 = Instagram.tag_recent_media((params[:hashword2]), {:count => 15, :client_id => ENV['INSTAAPI_CLIENT_ID']})
    	  end
-
       if current_user
-      # binding.pry
         @feed = Instagram.user_media_feed(:access_token => current_user.access_token, :count => 15, :client_id => ENV['INSTAAPI_CLIENT_ID'])
       end
-
-
-
-
+# below is the code that inputs the above feeds or search inputs into the javascript controllers
       @feedurls = []
       if @current_user.present?
-      @feed.each do |feed|
-        @feedurls.push(feed.images.standard_resolution.url)
+        @feed.each do |feed|
+          @feedurls.push(feed.images.standard_resolution.url)
+            end
       end
-      @feedurls = @feedurls
-    end
 
       @usearch = []
       if @user_name2.present?
           @user_name2.each do |u|
-          @usearch.push(u.images.standard_resolution.url)
-        end
+            @usearch.push(u.images.standard_resolution.url)
+          end
       end
-      @usearch = @usearch
 
       @utags = []
       if @tags.present?
-      @tags.each do |u|
-        @utags.push(u.images.standard_resolution.url)
+        @tags.each do |u|
+          @utags.push(u.images.standard_resolution.url)
+            end
       end
-    end
-    @tags = @tags
-
-
   end
 
   private
@@ -66,6 +52,7 @@ class HomeController < ApplicationController
   def get_user_id
       @user = current_user
   end
+  
 end 
 
 
