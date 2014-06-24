4aa0803a36d995aad45be0cb5e936ec7b7b0bbea
Andrew Donovan <andrewdonovan@Andrews-MacBook-Air.local>
2014-06-23 21:23:52 -0700

    added hover only feature for save and delete

---
+++ app/assets/javascripts/pictures.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.inside').hover(
+    function(){
+        $('.del-button', this).removeClass('del-button-hide');
+    },
+    function(){
+        $('.del-button', this).addClass('del-button-hide');
+    });
+
+});
+
+$(document).ready(function(){
+  $('.inside').hover(
+    function(){
+        $('.del-button', this).removeClass('del-button-hide');
+    },
+    function(){
+        $('.del-button', this).addClass('del-button-hide');
+    });
+
+});
