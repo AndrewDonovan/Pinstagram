b42622e6017742413f05e8d737379adae6bc50ff
Andrew Donovan <andrewdonovan@Andrews-MacBook-Air.local>
2014-06-25 19:59:39 -0700

    precompiled and added icons to every link

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,8 +1,8 @@
 describe HomeController do
 	describe "GET index" do
-    		it "has a 200 status code" do
+    		it "has a 302 status code" do
       			get :index
-      			expect(response.status).to eq(200)
+      			expect(response.status).to eq(302)
   		end
   	end
 end
